6c5e5af2fffddc262929760668d689575636e8a1
Arcbel <abramirezc@unal.edu.co>
2024-09-12 08:11:04 -0500

    MediaQ, ajuste express

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-// server.js
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-//ruta principal donde el navehador nos mostrara un mensaje de "Hola mundo"
 app.use(express.static('public'));
 
-//Inicia el servidor
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
